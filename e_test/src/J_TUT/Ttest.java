8c3e194b277e72d0950bdbc46c9f80ee4d8a4990
Waan-ofii <tasfayeayele636@gmail.com>
2025-08-14 22:31:29 +0300

    Testing a code

---
+++ e_test/src/J_TUT/Ttest.java
@@ -0,0 +1,17 @@
+package J_TUT;
+
+public class Ttest {
+    public void ok() {
+        double i = 50.0;
+        double k = i + 50.0;
+        double j = k + 1;
+
+        System.out.println("j is " + j + " and 8 k is " + k);
+        System.out.println("java");
+        System.out.println("c++");
+        System.out.println("python");
+        System.out.print("java");
+        System.out.print("c++");
+        System.out.print("python");
+                 }
+}
---
+++ e_test/src/J_TUT/test.java
@@ -0,0 +1,11 @@
+package J_TUT;
+
+public class test {
+    public static void main(String [] args){
+        System.out.println("hello world");
+
+Ttest tobj=new Ttest();
+tobj.ok();
+
+    }
+}
