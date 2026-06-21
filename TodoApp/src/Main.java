580e33c5e7212e4ee5c34585e8ba61d584d2b88b
One Ofii <tasfayeayele636@gmail.com>
2026-06-21 21:53:18 +0300

    modified task in todo app

--- TodoApp/src/Main.java
+++ TodoApp/src/Main.java
@@ -1,2 +1,19 @@
+import ui.TodoFrame;
+import javax.swing.*;
+
 public class Main {
+    public static void main(String[] args) {
+        // Set Look and Feel to system default
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        // Run the GUI on the Event Dispatch Thread
+        SwingUtilities.invokeLater(() -> {
+            TodoFrame frame = new TodoFrame();
+            frame.setVisible(true);
+        });
+    }
 }
